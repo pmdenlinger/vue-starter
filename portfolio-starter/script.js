c8a3aa243e37edc3bc5b10222c0d3985e6c03f44
Paul Denlinger <pmdenlinger@icloud.com>
2026-03-16 09:23:47 -0700

    Add looping for array objects in blog

--- portfolio-starter/script.js
+++ portfolio-starter/script.js
@@ -6,4 +6,28 @@ Vue.createApp({
         };
     },
 }).mount("header")
-Vue.createApp().mount("#blog")
+
+const posts = [
+                {
+                    id:1,
+                    title: "Why I learned Vue",
+                    body: "I'm baby chambray street art thundercats occupy four loko church-key disrupt. Shaman neutra bushwick chicharonnes",
+                },
+                {
+                    id:2,
+                    title: "Using the Vue CDN",
+                    body: "I'm baby chambray street art thundercats occupy four loko church-key disrupt. Shaman neutra bushwick chicharonnes",
+                },
+                {
+                    id:3, 
+                    title: "How I mastered Vue",
+                    body: "I'm baby chambray street art thundercats occupy four loko church-key disrupt. Shaman neutra bushwick chicharonnes",
+                },
+            ]
+Vue.createApp({
+    data(){
+        return {
+            posts,
+        };
+    },
+}).mount("#blog");
