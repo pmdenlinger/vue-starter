d814bf46de07ba8e3d3a3d52bf7ea0f7af2817c9
Paul Denlinger <pmdenlinger@icloud.com>
2026-03-17 09:27:47 -0700

    Add darkMode to ternary in index.html

--- portfolio-starter/script.js
+++ portfolio-starter/script.js
@@ -1,39 +1,35 @@
 const links = [
-    { id:1, name:"home", url:"index.html"},
-    { id:2, name:"portfolio", url:"portfolio.html" },
-    { id: 3, name:"contact me", url:"contact.html"}
+  { id: 1, name: 'home', url: 'index.html' },
+  { id: 2, name: 'portfolio', url: 'portfolio.html' },
+  { id: 3, name: 'contact me', url: 'contact.html' },
 ];
 
-
 const posts = [
-                {
-                    id:1,
-                    title: "Why I learned Vue",
-                    body: "I'm baby chambray street art thundercats occupy four loko church-key disrupt. Shaman neutra bushwick chicharonnes",
-                },
-                {
-                    id:2,
-                    title: "Using the Vue CDN",
-                    body: "I'm baby chambray street art thundercats occupy four loko church-key disrupt. Shaman neutra bushwick chicharonnes",
-                },
-                {
-                    id:3, 
-                    title: "How I mastered Vue",
-                    body: "I'm baby chambray street art thundercats occupy four loko church-key disrupt. Shaman neutra bushwick chicharonnes",
-                },
-            ]
-
-
+  {
+    id: 1,
+    title: 'Why I learned Vue',
+    body: "I'm baby chambray street art thundercats occupy four loko church-key disrupt. Shaman neutra bushwick chicharonnes",
+  },
+  {
+    id: 2,
+    title: 'Using the Vue CDN',
+    body: "I'm baby chambray street art thundercats occupy four loko church-key disrupt. Shaman neutra bushwick chicharonnes",
+  },
+  {
+    id: 3,
+    title: 'How I mastered Vue',
+    body: "I'm baby chambray street art thundercats occupy four loko church-key disrupt. Shaman neutra bushwick chicharonnes",
+  },
+];
 
 Vue.createApp({
-    data() {
-        return {
-            name: "Chris Dixon",
-            links,
-            posts,
-            darkModeSet: true,
-            textColor:"hotpink"
-        };
-    },
-}).mount("body");
-
+  data() {
+    return {
+      name: 'Chris Dixon',
+      links,
+      posts,
+      darkModeSet: false,
+      textColor: 'hotpink',
+    };
+  },
+}).mount('body');
